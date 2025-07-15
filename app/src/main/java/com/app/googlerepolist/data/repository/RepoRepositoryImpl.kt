eeb1adaebeae3d0dbf4a8136693260a95c775c0f
Mohamad Jamous <mohamad.e.jamous@gmail.com>
2025-07-15 10:40:32 +0300

    feat: add caching in repo implementation

--- app/src/main/java/com/app/googlerepolist/data/repository/RepoRepositoryImpl.kt
+++ app/src/main/java/com/app/googlerepolist/data/repository/RepoRepositoryImpl.kt
@@ -6,15 +6,27 @@ import com.app.googlerepolist.data.remote.dto.RepoDto
 import com.app.googlerepolist.domain.repository.RepoRepository
 import javax.inject.Inject
 
-class RepoRepositoryImpl @Inject constructor(
-    private val api : GoogleRepoApi
+class RepoRepositoryImpl(
+    private val api: GoogleRepoApi
 ) : RepoRepository {
 
+    private var repoCache: List<RepoDto>? = null
+
     override suspend fun getRepos(): List<RepoDto> {
-        return api.getRepos()
+        repoCache?.let {
+            return it
+        }
+
+        val repos = api.getRepos()
+        repoCache = repos
+        return repos
     }
 
-    override suspend fun getRepoBydId(repoId: String): RepoDetailDto {
-        return api.getRepoById(repoId = repoId)
+    override suspend fun getRepoBydId(id: String): RepoDetailDto {
+        return api.getRepoById(id)
+    }
+
+    fun clearCache() {
+        repoCache = null
     }
 }
